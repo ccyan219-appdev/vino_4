80a2ce07638d6e8d4ef6011bb24a016433fd1dfa
Raghu Betina <raghu@firstdraft.com>
2018-06-17 02:09:36 +0000

    Indirect association between users and locations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,10 @@ class User < ApplicationRecord
 
   # Indirect associations
 
+  has_many   :locations,
+             :through => :histories,
+             :source => :location
+
   has_many   :wines,
              :through => :histories,
              :source => :wine
